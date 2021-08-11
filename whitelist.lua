a76675a9afa8c0cc73a4b1b2f58f236fd6e09096
Sniperkaos <40675557+Sniperkaos@users.noreply.github.com>
2021-08-11 19:07:10 -0400

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -3,7 +3,6 @@ return {
 	["doggyshot123"] = true,
 	["xayee"] = true,
 	["legendarysniper203"] = true,
-  ["dynamitebirthday"] = true,
-  ["gavinthelordofdeath"] = true
-
+  	["dynamitebirthday"] = true,
+  	["gavinthelordofdeath"] = true
 }
