c83c3b24a2aaf30bb87a536123dbb3ee6865f8bb
Sniperkaos <40675557+Sniperkaos@users.noreply.github.com>
2021-08-11 19:06:15 -0400

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,9 @@
+return {
+	["sniperkaos"] = true,
+	["doggyshot123"] = true,
+	["xayee"] = true,
+	["legendarysniper203"] = true,
+  ["dynamitebirthday"] = true,
+  ["gavinthelordofdeath"] = true
+
+}
